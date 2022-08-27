a67a767911526fcc8475cc72fe86a564a60e1415
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-08-26 23:36:42 -0300

    Atualizado Remove Despesa

--- src/main/java/br/com/codandosimples/RemoverDespesa.java
+++ src/main/java/br/com/codandosimples/RemoverDespesa.java
@@ -1,6 +1,7 @@
 package br.com.codandosimples;
 
 import br.com.codandosimples.dao.DespesaDAO;
+import br.com.codandosimples.model.Despesa;
 
 public class RemoverDespesa {
     public static void main(String[] args) {
