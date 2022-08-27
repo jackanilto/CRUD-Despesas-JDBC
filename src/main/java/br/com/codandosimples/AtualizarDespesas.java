3f969837ba6293216bdaffe0b438aaa402934ca6
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-08-26 23:52:12 -0300

    Atualizado AtualizaDespesas

--- src/main/java/br/com/codandosimples/AtualizarDespesas.java
+++ src/main/java/br/com/codandosimples/AtualizarDespesas.java
@@ -18,14 +18,6 @@ public class AtualizarDespesas {
         System.out.println(despesa.getData());
         System.out.println(despesa.getValor());
         System.out.println(despesa.getCategoria());
-
-//        despesa.setDescricao("Dentista");
-//        despesa.setData(LocalDate.of(2022, 8, 24));
-//        despesa.setValor(235.65);
-//        despesa.setCategoria(Categoria.OUTROS);
-//
-//        dao.update(despesa);
-
     }
 
 
